ef64905965d7b3ff497b879483a5373ba797c9d2
gautham elango <gautham@gcubed.co>
2017-05-13 18:29:45 +0900

    fixes

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.R)) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+	}
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/color.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color : MonoBehaviour {
+
+	public float timer = 0.0f;
+
+	void Start () 
+	{
+		
+	}
+
+
+	void Update () 
+	{
+		timer += Time.deltaTime;
+		if (timer >= 0.5f)//change the float value here to change how long it takes to switch.
+		{
+			// pick a random color
+			Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
+			// apply it on current object's material
+			GetComponent<Renderer>().material.color = newColor;
+			timer = 0;
+		}
+
+
+	}
+}
